8201945227ec41fc69dcada857c26a38677f4ea3
erhan paker <erhanpaker@gmail.com>
2023-02-25 14:46:24 +0300

    testNg Practies

--- src/test/java/testNg_framework/priority.java
+++ src/test/java/testNg_framework/priority.java
@@ -1,2 +1,42 @@
-package testNg_framework;public class priority {
+package testNg_framework;
+
+import org.testng.annotations.Test;
+
+public class priority {
+
+    /*
+     testNg testleri isim sırasına göre çalıştırır.
+
+     */
+
+    @Test (priority = 5)
+
+    public void amazonTest(){
+        System.out.println("amazon");
+
+    }
+
+    @Test (priority = -7)
+
+    public void youTubeTest(){
+        System.out.println("youtube");
+
+    }
+
+    @Test
+
+    public void wisequearterTest(){
+        System.out.println("wisequertar");
+
+    }
+
+    @Test
+
+    public void bestBuyTest(){
+        System.out.println("bestbuy");
+
+    }
+
+
+
 }
