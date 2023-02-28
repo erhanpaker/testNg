2790a4449ac5b3ba5e78c96b3cb676579627482e
erhan paker <erhanpaker@gmail.com>
2023-02-28 15:25:55 +0300

    testng

--- src/test/java/pages/QualitydemyPage.java
+++ src/test/java/pages/QualitydemyPage.java
@@ -1,10 +1,18 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.testng.annotations.Test;
 
 public class QualitydemyPage  {
 
-    //  Hocam findBy notasyonu gelmiyor.  hatayı da bulamadım
+    @FindBy(linkText = "Log in")
+    public WebElement ilkLoginLinki;
+
+    @FindBy(id = "login-email")
+    public WebElement emailKutusu;
+
+
 
 
 
