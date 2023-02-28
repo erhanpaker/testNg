82ca52303aadc4a9a24f67be38584c7f2d3cb9b7
erhan paker <erhanpaker@gmail.com>
2023-02-28 15:51:28 +0300

    testng

---
+++ src/test/java/testNg_framework/ilkTest.java
@@ -0,0 +1,14 @@
+package testNg_framework;
+
+import org.testng.annotations.Test;
+import utilities.Driver;
+
+public class ilkTest {
+
+    @Test
+
+    public void test1(){
+
+        Driver.getDriver().get("https://qualitydemy.com/");
+    }
+}
---
+++ src/test/java/utilities/Driver.java
@@ -0,0 +1,22 @@
+package utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class Driver {
+
+   public static WebDriver driver;
+
+    public static WebDriver getDriver(){
+
+        WebDriverManager.chromedriver().setup();
+        driver= new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.manage().window().maximize();
+
+        return driver;
+    }
+}
