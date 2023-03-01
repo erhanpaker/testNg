82f5677436ba5a2981794b168a7c3341d4fd0cdb
erhan paker <erhanpaker@gmail.com>
2023-03-01 15:33:31 +0300

    qualitydemy

--- src/test/java/pages/QualitydemyPage.java
+++ src/test/java/pages/QualitydemyPage.java
@@ -2,9 +2,15 @@ package pages;
 
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.Test;
+import utilities.Driver;
 
 public class QualitydemyPage  {
+
+    public QualitydemyPage() {
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
 
     @FindBy(linkText = "Log in")
     public WebElement ilkLoginLinki;
--- src/test/java/testNg_framework/ilkTest.java
+++ src/test/java/testNg_framework/ilkTest.java
@@ -1,6 +1,7 @@
 package testNg_framework;
 
 import org.testng.annotations.Test;
+import pages.QualitydemyPage;
 import utilities.Driver;
 
 public class ilkTest {
@@ -10,6 +11,9 @@ public class ilkTest {
     public void test1(){
 
         Driver.getDriver().get("https://qualitydemy.com/");
+
+        QualitydemyPage qualitydemyPage = new QualitydemyPage();
+        qualitydemyPage.ilkLoginLinki.click();
 
         Driver.closeDriver();
     }
