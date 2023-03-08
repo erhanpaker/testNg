6bd49f156de2d0482415e918ff4967728fa582ff
erhan <erhanpaker@gmail.com>
2023-03-08 13:37:18 +0300

    testeee

--- src/test/java/pages/ders12_assertions/test.java
+++ src/test/java/pages/ders12_assertions/test.java
@@ -4,4 +4,8 @@ public class test {
     public static void main(String[] args) {
         System.out.println("erhan");
     }
+
+    public static void main(String[] args) {
+        System.out.println("erhan");
+    }
 }
